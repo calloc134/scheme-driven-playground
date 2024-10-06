3e10f5179d7658ba4c1068586dd2161ddbc99b0b
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-10-06 10:05:12 +0900

    フロントエンド orval構成ファイル追加

---
+++ openapi-hono/frontend/orval.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "orval";
+
+export default defineConfig({
+  posts: {
+    output: {
+      mode: "split",
+      target: "src/hooks/query/query.ts",
+      schemas: "src/types",
+      // TanStack Query(React Query)を使う
+      client: "react-query",
+      // 内部HTTPクライアントをfetchに指定
+      httpClient: "fetch",
+      // バックエンドのホストを指定
+      baseUrl: "http://localhost:3777",
+      // 以下はSuspenseを使う場合有効にする
+      //   override: {
+      //     query: {
+      //       useSuspenseQuery: true,
+      //     },
+      //   },
+    },
+    input: {
+      target: "../openapi.yaml",
+    },
+  },
+});
