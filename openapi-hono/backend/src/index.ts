223277a2a4e72b2c18e47cffdf75a737a4024467
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-10-07 11:59:04 +0900

    corsミドルウェア設定

--- openapi-hono/backend/src/index.ts
+++ openapi-hono/backend/src/index.ts
@@ -1,8 +1,18 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
 import generatedApp from "./generated/posts";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+app.use(
+  "*",
+  cors({
+    origin: "*", // とりあえず全てのリクエストを許可
+    allowMethods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.route("/", generatedApp);
 
